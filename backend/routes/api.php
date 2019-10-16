8b54ec409e1be45b938ee6837965d87f5dddf902
AugustineTreezy <augustinetreezy@outlook.com>
2019-10-16 22:49:18 +0300

    get all users from db

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,5 +22,6 @@ Route::prefix('auth')->group(function () {
     Route::group(['middleware' => 'auth:api'], function(){
         Route::get('user', 'AuthController@user');
         Route::post('logout', 'AuthController@logout');
+        Route::get('users', 'UserController@index');
     });
 });
